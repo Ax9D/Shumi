121ae8c9eef89ab6571946e4de0948725d5a2bd8
At <)>
2020-03-05 21:23:37 +0530

    Improved Collision Response system

--- src/game/BPhysics.java
+++ src/game/BPhysics.java
@@ -13,6 +13,59 @@ public class BPhysics {
 		bA.compute();
 		bB.compute();
 
+		float xoverlap;
+		float yoverlap;
+
+		float rightgap,leftgap,topgap,bottomgap;
+		boolean pushLeft;
+		boolean pushBottom;
+		if(isCollision(bA,bB))
+		{
+			rightgap=Math.abs(bA.right-bB.left);
+			leftgap=Math.abs(bA.left-bB.right);
+			topgap=Math.abs(bA.bottom-bB.top);
+			bottomgap=Math.abs(bA.top-bB.bottom);
+
+
+			if(rightgap<leftgap) {
+				xoverlap = rightgap;
+				pushLeft=true;
+			}
+			else
+			{
+				xoverlap=leftgap;
+				pushLeft=false;
+			}
+
+
+			if(bottomgap<topgap) {
+				yoverlap = bottomgap;
+				pushBottom=true;
+			}
+			else
+			{
+				yoverlap=topgap;
+				pushBottom=false;
+			}
+
+			if(xoverlap<yoverlap)
+			{
+				if(pushLeft)
+					A.pos.x-=xoverlap;
+				else
+					A.pos.x+=xoverlap;
+			}
+			else
+			{
+				if(pushBottom)
+					A.pos.y-=yoverlap;
+				else
+					A.pos.y+=yoverlap;
+			}
+		}
+
+
+		/*
 		if(isCollision(bA,bB))
 		{
 
@@ -38,11 +91,11 @@ public class BPhysics {
 
 
 
-		}
+		}*/
 	}
 	public static boolean isCollision(BoundingBox bA,BoundingBox bB)
 	{
-			return !(bA.right<=bB.left || bA.left>=bB.right || bA.bottom>=bB.top || bA.top<=bB.bottom);
+		return !(bA.right<=bB.left || bA.left>=bB.right || bA.bottom>=bB.top || bA.top<=bB.bottom);
 	}
 	public static boolean isCollision(float aleft,float aright,float bleft,float bright,float abottom,float atop,float bbottom,float btop)
 	{
